6661642dfe4cfb82ba282d925945faa3f3f5c574
Chris Meyer <cmeyer1969@gmail.com>
2016-05-12 14:53:41 -0700

    Update setup.py with dependencies and better classifiers.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import setuptools
-import os
 
 setuptools.setup(
     name="nion.typeshed",
     version="0.0.1",
     packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['numpy'],
+    install_requires=['niondata', 'nionutils', 'numpy'],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     include_package_data=True
 )

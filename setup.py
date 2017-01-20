c2bb1fb1f0fcfa687d3846cc638905f1b0750c90
Chris Meyer <cmeyer1969@gmail.com>
2017-01-20 15:11:44 -0800

    Update license to Apache License 2.0.

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ import setuptools
 setuptools.setup(
     name="nion.typeshed",
     version="0.0.1",
+    url="https://github.com/nion-software/typeshed",
     packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['niondata', 'nionutils', 'numpy'],
+    license='Apache 2.0',
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
         "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.5",
     ],
     include_package_data=True
 )
